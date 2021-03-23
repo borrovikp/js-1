4d35b55ec1f43c5749958272f467bbf033b66674
b <borrovikp@gmail.com>
2021-03-23 12:59:11 +0200

    gh-pages

---
+++ js/index.js
@@ -0,0 +1,8 @@
+const clockContainer = document.querySelector('.clock');
+
+setInterval(updateClock, 1000);
+
+function updateClock() {
+    clockContainer.innerText = new Date().toLocaleTimeString();
+}
+
